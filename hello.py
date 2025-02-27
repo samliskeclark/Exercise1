5d2e3cfa8970a7654fc5f999daba9b583ab4a8a8
samliskeclark <sjl9823@nyu.edu>
2025-02-27 09:42:16 +0400

    Add simple python code

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("Hello World!")
 print("I am editing a file so I can push the change to the repository.")
 print("This is a line written after entering git add hello.py")
+print("this is another change")
